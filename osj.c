3048754084d3ad9976f0ddf13f83525192f7c14d
junsuPark <krpjs0508@gmail.com>
2023-04-07 11:54:13 +0900

    aa

--- osj.c
+++ osj.c
@@ -11,19 +11,23 @@ int main() {
 	pid_t pid;
 	pid = fork();
 
-	int count = 1;
-
 	if (pid > num) { // 부모
 
+		int count = 0;
+		
 		for (int i = 1; i <= num; i++)
 			count += i;
 		
 		printf("parent process, %d\n", count);
 
 	} else {
+	
+		int count = 1;
 
-		for (int i = 1; i <= num; i++)
+		for (int i = 1; i <= num; i++) {
 			count *= i;
+			printf("%d %d\n", i, count);
+		}
 		printf("child process, %d\n", count);
 
 	}
